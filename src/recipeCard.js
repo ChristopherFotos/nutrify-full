06e01037275eab84a3462793a85a7af02a5b9817
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-04-27 17:14:51 -0400

    Add files via upload
    Card content is now wrapped in a div. This means that any number or type of elements can be put in the card and it will behave the same way.

--- src/recipeCard.js
+++ src/recipeCard.js
@@ -11,11 +11,23 @@ const RecipeCard = (props) => {
       <span className="recipe-title">{props.recipe.recipe.label}</span>
       <div className="card-content">
         <span className="show-details show-details-open">Show details</span>
-        <ul className="card-content-closed">
-          {props.recipe.recipe.ingredientLines.map((ingredientLine) => {
-            return <li>{ingredientLine}</li>;
-          })}
-        </ul>
+        <div className="card-content-closed">
+          <ul>
+            {props.recipe.recipe.ingredientLines.map((ingredientLine) => {
+              return <li>{ingredientLine}</li>;
+            })}
+          </ul>
+          <h4>A lovely heading</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
         <div className="recipe-summary-div">
           6 ingredients | Low carb | Low fat | Martha Stewart
         </div>
